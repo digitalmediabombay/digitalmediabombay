b0750aa60cd4d9651ddece43529377939cd7a738
digitalmediabombay <digitalmediabombay@gmail.com>
2026-01-19 13:21:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-pnimport React, { useState, useEffect, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import { Analytics } from '@vercel/analytics/react';
 
 import { 
