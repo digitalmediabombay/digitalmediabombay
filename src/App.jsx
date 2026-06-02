2b9a44124029c6dc6d1eae3ba21bdb9a6d26051d
Digital Media Bombay <digitalmediabombay@gmail.com>
2026-06-02 23:52:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -6088,7 +6088,7 @@ const CookieBanner = () => {
     </div>
   );
 };
-const FullPricingPage = ({ pricingMode, currencySymbol, openBundles, onContactClick }) => {
+const FullPricingPage = ({ pricinfunction FullPricingPage({ pricingMode, currencySymbol, openBundles, onContactClick }) {gMode, currencySymbol, openBundles, onContactClick }) => {
   
   const exclusiveStandaloneServices = [
     {
